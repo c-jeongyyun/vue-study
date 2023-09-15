7da80bd670aa6f86cd95954cf2d489dd48e2c90e
c-jeongyyun <99jenny0614@naver.com>
2023-09-15 21:46:40 +0900

    👔 add conditional rendering and list rendering

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const app = Vue.createApp({
         return {
             product:"Socks",
             image:"./assets/images/socks_blue.jpg",
-            githubUrl:"https://github.com/c-jeongyyun"
+            githubUrl:"https://github.com/c-jeongyyun",
+            inventory:8,
+            onSale:true,
+            details:[
+                "면 20%", 
+                "폴리에스터 70%", 
+                "울 10%", 
+            ],
+            variants:[
+                {id:"124", color:"green"},
+                {id:"125", color:"blue"}
+            ],
+            sizes:[
+                "S","M","L"
+            ],
+            cart:0,
+
+        
+        
         }
     }
 })
