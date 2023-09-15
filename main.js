bdf9198724e0e289b0aa14b6e8087d30ed95bce8
c-jeongyyun <99jenny0614@naver.com>
2023-09-16 01:22:53 +0900

    👔 add class & style binding

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
 const app = Vue.createApp({
-    data(){
-        return {
-            product:"Socks",
-            image:"./assets/images/socks_blue.jpg",
-            githubUrl:"https://github.com/c-jeongyyun",
-            inventory:8,
-            onSale:true,
-            details:[
-                "면 20%", 
-                "폴리에스터 70%", 
-                "울 10%", 
-            ],
-            variants:[
-                {id:"124", color:"green", image:"./assets/images/socks_green.jpg" },
-                {id:"125", color:"blue", image:"./assets/images/socks_blue.jpg" }
-            ],
-            sizes:[
-                "S","M","L"
-            ],
-            cart:0,
-        }
-       
-    }, 
-    methods: {
-        removeFromCart(){
-            this.cart-=1;
-        },
-        updateImage(variantImage){
-            this.image = variantImage;
-        }
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      githubUrl: "https://github.com/c-jeongyyun",
+      inventory: 8,
+      onSale: true,
+      details: ["면 20%", "폴리에스터 70%", "울 10%"],
+      variants: [
+        { id: "124", color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: "125", color: "blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+      sizes: ["S", "M", "L"],
+      cart: 0,
+    };
+  },
+  methods: {
+    removeFromCart() {
+      if (this.cart > 0) this.cart -= 1;
     },
-})
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+});
