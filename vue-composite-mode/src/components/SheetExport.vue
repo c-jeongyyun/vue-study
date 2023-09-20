503aff5190178a7ba561f4a36c3de2a38a9c0bca
c-jeongyyun <99jenny0614@naver.com>
2023-09-20 22:27:10 +0900

    🚚 replace vue project to yarn

---
+++ vue-composite-mode/src/components/SheetExport.vue
@@ -0,0 +1,10 @@
+<script setup lang='ts'>
+import type { read } from "xlsx";
+
+</script>
+
+<template>
+<div></div>
+</template>
+
+<style scoped></style>
---
+++ vue-npm-project/src/components/SheetImport.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { read } from "xlsx";
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
