ea406257fdf4367e7e4d250f9aa4fd1e92df99e1
c-jeongyyun <99jenny0614@naver.com>
2024-02-23 21:44:31 +0900

    🚧 make circle progress

---
+++ vue-composite-mode/src/components/progress/CircleProgress.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="circle"></div>
+</template>
+
+<script setup lang="ts">
+// *prop
+
+/** prop
+ * * 굵기
+ * * 시작 퍼센트
+ * * 종료 퍼센트
+ * * 끝 부분 동그랗게 할지말지
+ * * 애니메이션 할지 말지
+ *
+ */
+</script>
+
+<style scoped lang="scss">
+.circle {
+  position: relative;
+  // background: purple;
+
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+
+  border: 1px solid black;
+
+  &::before {
+    content: "";
+    position: absolute;
+    border-radius: 50%;
+    inset: 0;
+    background: conic-gradient(hotpink 90%, transparent 0);
+    mask: radial-gradient(closest-side, transparent 80%, white 80% 100%);
+  }
+}
+</style>
---
+++ vue-composite-mode/src/views/ProgressUiView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CircleProgress />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CircleProgress from "@/components/progress/CircleProgress.vue";
+</script>
+
+<style scoped lang="scss"></style>
